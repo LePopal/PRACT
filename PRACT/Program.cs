1b26af4be821a18e570bb1ec35909e2af65b7839
Popal <popal@sboub.com>
2018-03-01 23:39:49 +0100

    First version on GitHub

--- PRACT/Program.cs
+++ PRACT/Program.cs
@@ -17,189 +17,6 @@ namespace PRACT
     {
         static void Main(string[] args)
         {
-            //XDocument xdoc = 
-            //    XDocument.Load(@"C:\Users\Axel\source\repos\PRACT\PRACT\Samples\pc.xml");
-
-            //DJ_PLAYLISTS MyRkbXml = new DJ_PLAYLISTS();
-            //MyRkbXml =
-            //    (from x in xdoc.Descendants("DJ_PLAYLISTS")
-            //     select new DJ_PLAYLISTS
-            //     {
-            //         Version = (string)x.Attribute("Version"),
-            //         Product =
-            //         (from p in x.Descendants("PRODUCT")
-            //          select new PRODUCT
-            //          {
-            //              Company = (string)p.Attribute("Company"),
-            //              Name = (string)p.Attribute("Name"),
-            //              Version = (string)p.Attribute("Version")
-            //          }).First(),
-            //         Collection =
-            //         (from c in x.Descendants("COLLECTION")
-            //          select new COLLECTION
-            //          {
-            //              Entries = (int)c.Attribute("Entries"),
-            //              Tracks =
-            //              (from e in c.Descendants("TRACK")
-            //               select new TRACK
-            //               {
-            //                   Album = (string)e.Attribute("Album"),
-            //                   Artist = (string)e.Attribute("Artist"),
-            //                   AverageBpm = (float)e.Attribute("AverageBpm"),
-            //                   BitRate = (int)e.Attribute("BitRate"),
-            //                   Color = (string)e.Attribute("Color"),
-            //                   Comments = (string)e.Attribute("Comments"),
-            //                   Composer = (string)e.Attribute("Composer"),
-            //                   DateAdded = (DateTime)e.Attribute("DateAdded"),
-            //                   DiscNumber = (int)e.Attribute("DiscNumber"),
-            //                   Genre = (string)e.Attribute("Genre"),
-            //                   Grouping = (string)e.Attribute("Grouping"),
-            //                   Kind = (string)e.Attribute("Kind"),
-            //                   Label = (string)e.Attribute("Label"),
-            //                   Location = (string)e.Attribute("Location"),
-            //                   Mix = (string)e.Attribute("Mix"),
-            //                   Name = (string)e.Attribute("Name"),
-            //                   PlayCount = (int)e.Attribute("PlayCount"),
-            //                   Rating = (int)e.Attribute("Rating"),
-            //                   Remixer = (string)e.Attribute("Remixer"),
-            //                   SampleRate = (int)e.Attribute("SampleRate"),
-            //                   Size = (int)e.Attribute("Size"),
-            //                   Tonality = (string)e.Attribute("Tonality"),
-            //                   TotalTime = (int)e.Attribute("TotalTime"),
-            //                   TrackID = (int)e.Attribute("TrackID"),
-            //                   TrackNumber = (int)e.Attribute("TrackNumber"),
-            //                   Year = (int)e.Attribute("Year"),
-            //                   PositionMark =
-            //        (
-            //            from p in e.Elements("POSITION_MARK")
-            //            select new POSITION_MARK
-            //            {
-            //                Blue = (int?)p.Attribute("Blue"),
-            //                End = (float?)p.Attribute("End"),
-            //                Green = (int?)p.Attribute("Green"),
-            //                Name = (string)p.Attribute("Name"),
-            //                Num = (int)p.Attribute("Num"),
-            //                Red = (int?)p.Attribute("Red"),
-            //                Start = (float)p.Attribute("Start"),
-            //                Type = (int)p.Attribute("Type")
-
-            //            }
-            //        ).ToArray(),
-            //                   Tempo =
-            //        (
-            //        from t in e.Elements("TEMPO")
-            //        select new TEMPO
-            //        {
-            //            Battito = (int)t.Attribute("Battito"),
-            //            Bpm = (float)t.Attribute("Bpm"),
-            //            Inizio = (float)t.Attribute("Inizio"),
-            //            Metro = (string)t.Attribute("Metro")
-            //        }
-            //        ).ToArray()
-            //               }
-            //               ).ToArray()
-
-            //          }
-            //         ).First(),
-            //         Playlists =(
-            //                         from e in x.Descendants("PLAYLISTS").Descendants("NODE")
-            //                         select new NODE
-            //                         {
-            //                             Count = (int?)e.Attribute("Count"),
-            //                             Entries = (int?)e.Attribute("Entries"),
-            //                             KeyType = (int?)e.Attribute("KeyType"),
-            //                             Name = (string)e.Attribute("Name"),
-            //                             Type = (int)e.Attribute("Type"),
-            //                             Tracks = (
-            //                             from t in e.Elements("TRACK")
-            //                             select new TRACKNODE
-            //                             {
-            //                                 Key = (int)t.Attribute("Key")
-            //                             }
-            //                             ).ToArray()
-            //                         }
-            //    ).ToArray()
-            //     }).First();
-
-            //MyRkbXml.Collection.Tracks = (
-            //    from e in xdoc.Descendants("DJ_PLAYLISTS").Descendants("COLLECTION").Descendants("TRACK")
-            //    select new TRACK
-            //    {
-            //        Album = (string)e.Attribute("Album"),
-            //        Artist = (string)e.Attribute("Artist"),
-            //        AverageBpm = (float)e.Attribute("AverageBpm"),
-            //        BitRate = (int)e.Attribute("BitRate"),
-            //        Color = (string)e.Attribute("Color"),
-            //        Comments = (string)e.Attribute("Comments"),
-            //        Composer = (string)e.Attribute("Composer"),
-            //        DateAdded = (DateTime)e.Attribute("DateAdded"),
-            //        DiscNumber = (int)e.Attribute("DiscNumber"),
-            //        Genre = (string)e.Attribute("Genre"),
-            //        Grouping = (string)e.Attribute("Grouping"),
-            //        Kind = (string)e.Attribute("Kind"),
-            //        Label = (string)e.Attribute("Label"),
-            //        Location = (string)e.Attribute("Location"),
-            //        Mix = (string)e.Attribute("Mix"),
-            //        Name = (string)e.Attribute("Name"),
-            //        PlayCount = (int)e.Attribute("PlayCount"),
-            //        Rating = (int)e.Attribute("Rating"),
-            //        Remixer = (string)e.Attribute("Remixer"),
-            //        SampleRate = (int)e.Attribute("SampleRate"),
-            //        Size = (int)e.Attribute("Size"),
-            //        Tonality = (string)e.Attribute("Tonality"),
-            //        TotalTime = (int)e.Attribute("TotalTime"),
-            //        TrackID = (int)e.Attribute("TrackID"),
-            //        TrackNumber = (int)e.Attribute("TrackNumber"),
-            //        Year = (int)e.Attribute("Year"),
-            //        PositionMark =
-            //        (
-            //            from p in e.Elements("POSITION_MARK")
-            //            select new POSITION_MARK
-            //            {
-            //                Blue = (int?)p.Attribute("Blue"),
-            //                End  = (float?)p.Attribute("End"),
-            //                Green = (int?)p.Attribute("Green"),
-            //                Name = (string)p.Attribute("Name"),
-            //                Num = (int)p.Attribute("Num"),
-            //                Red = (int?)p.Attribute("Red"),
-            //                Start = (float)p.Attribute("Start"),
-            //                Type = (int)p.Attribute("Type")
-
-            //            }
-            //        ).ToArray(),
-            //        Tempo =
-            //        (
-            //        from t in e.Elements("TEMPO")
-            //        select new TEMPO
-            //        {
-            //            Battito = (int)t.Attribute("Battito"),
-            //            Bpm=(float)t.Attribute("Bpm"),
-            //            Inizio=(float)t.Attribute("Inizio"),
-            //            Metro=(string)t.Attribute("Metro")
-            //        }
-            //        ).ToArray()
-            //    }
-
-            //    ).ToArray();
-
-            //MyRkbXml.Playlists = (
-            //    from e in xdoc.Descendants("DJ_PLAYLISTS").Descendants("PLAYLISTS").Descendants("NODE")
-            //    select new NODE
-            //    {
-            //        Count = (int?)e.Attribute("Count"),
-            //        Entries = (int?)e.Attribute("Entries"),
-            //        KeyType = (int?)e.Attribute("KeyType"),
-            //        Name = (string)e.Attribute("Name"),
-            //        Type = (int)e.Attribute("Type"),
-            //        Tracks = (
-            //        from t in e.Elements("TRACK")
-            //        select new TRACKNODE
-            //        {
-            //            Key= (int)t.Attribute("Key")
-            //        }
-            //        ).ToArray()
-            //    }
-            //    ).ToArray();
 
             Assembly assembly = Assembly.GetExecutingAssembly();
             FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
@@ -223,16 +40,21 @@ namespace PRACT
                     DJ_PLAYLISTS m2 = new DJ_PLAYLISTS(argRkbXml.Value());
                     string Destination;
                     Destination = Path.Combine(argOutputDir.Value(), "Orphans.m3u8");
-                    Console.WriteLine("Writing Orphans playlist in {0}...", Destination);
+                    Console.WriteLine("Writing Orphans playlist to {0}...", Destination);
                     PlaylistHelper.WritePlaylist(m2.Orphans, Destination);
                     Destination = Path.Combine(argOutputDir.Value(), "Duplicates.m3u8");
-                    Console.WriteLine("Writing Duplicates playlist in {0}...", Destination);
+                    Console.WriteLine("Writing Duplicates playlist to {0}...", Destination);
                     PlaylistHelper.WritePlaylist(m2.Duplicates, Destination);
                     Destination = Path.Combine(argOutputDir.Value(), "Unanalyzed.m3u8");
-                    Console.WriteLine("Writing Unanalyzed playlist in {0}...", Destination);
+                    Console.WriteLine("Writing Unanalyzed playlist to {0}...", Destination);
                     PlaylistHelper.WritePlaylist(m2.UnAnalyzed, Destination);
+                    Destination = Path.Combine(argOutputDir.Value(), "Missing.m3u8");
+                    Console.WriteLine("Writing Missing playlist to {0}...", Destination);
+                    PlaylistHelper.WritePlaylist(m2.Missing, Destination);
+                    Destination = Path.Combine(argOutputDir.Value(), "Untagged.m3u8");
+                    Console.WriteLine("Writing Untagged playlist to {0}...", Destination);
+                    PlaylistHelper.WritePlaylist(m2.Untagged, Destination);
                     Console.WriteLine("Job's finished !");
-                    //Console.WriteLine(argRkbXml.Value());
                     return 0;
                 }
                 else
@@ -244,10 +66,6 @@ namespace PRACT
             cmd.Name = "Name";
             cmd.HelpOption("-? | -h | --help");
             cmd.Execute(args);
-            
-
-
-            //Console.ReadLine();
         }
     }
 }
